5cf3690b1a00efe18b85b05de11492e7c0bc7c66
Himanshu Nandan <himanshukarn1999@gmail.com>
2022-02-20 18:14:32 +0530

    html change

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -1,13 +1,25 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '../index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
+const iio = socketIO(server);
+
 //node server which will handel socket io connections
 
-const io = require('socket.io')(8000, {
-    cors: {
-        origin: '*',
-      }
-});
+// const io = require('socket.io')(8000, {
+//     cors: {
+//         origin: '*',
+//       }
+// });
 const users = {};
 
-io.on('connection', socket =>{
+iio.on('connection', socket =>{
     socket.on('new-user-joined', name1 =>{
         //console.log("New user", name1);
         users[socket.id] = name1;

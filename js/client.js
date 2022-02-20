bdf4f11016ae8c22dea5bbb894f9af3353e1d641
Himanshu Nandan <himanshukarn1999@gmail.com>
2022-02-20 17:37:04 +0530

    socket

--- js/client.js
+++ js/client.js
@@ -1,3 +1,13 @@
+// const PORT = process.env.PORT || 3000;
+// const INDEX = '/index.html';
+
+// const server = express()
+//   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
+// const socket = io(server);
+
 const socket = io('http://localhost:8000');
 
 const form = document.getElementById('send-container');
